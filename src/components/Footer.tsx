ac77a8152b93a344b446033b43aff7385e7dc06b
Bruno Oliveira <brunooliver2015@outlook.com>
2025-11-12 16:37:50 -0300

    removi import do inicio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaWhatsapp, FaInstagram, FaFacebookF } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
